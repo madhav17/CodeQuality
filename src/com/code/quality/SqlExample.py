2a26f5950f0f07e2811ec4b394f0cfd11052bbbb
Madhav Khanna <madhav.khanna@tothenew.com>
2024-03-13 18:32:13 +0530

    SQL example added

--- src/com/code/quality/SqlExample.py
+++ src/com/code/quality/SqlExample.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import col
 import pyspark.sql.functions as func
 
 
-class SampleSql:
+class SqlExample:
     def __init__(self):
         self.__spark = (
             SparkSession.builder.master("local").appName("SampleSql").getOrCreate()
@@ -15,11 +15,13 @@ class SampleSql:
         df: pd.DateFrame = self.__spark.sql("SELECT 1").toPandas()
         return df.iloc[0, 0]
 
-    def create_sql_table(self, cus_df: list, table_name: str) -> "PandasDataFrameLike":
+    def create_sql_table(self, cus_df: list, table_name: str):
         sdf = self.__spark.createDataFrame(cus_df)
         sdf.createOrReplaceTempView(table_name)
-        pdfl: "PandasDataFrameLike" = self.__spark.sql(
-            f"SELECT name, age FROM {table_name}"
-        ).toPandas()
-        self.__spark.catalog.dropTempView(table_name)
-        return pdfl
+
+    def read_sql_table(self, query: str) -> pd.DataFrame:
+        df: pd.DataFrame = self.__spark.sql(query).toPandas()
+        return df
+
+    def drop_table(self, name: str):
+        self.__spark.catalog.dropTempView(name)  # Delete the table
--- tests/com/code/quality/test_sample_sql.py
+++ tests/com/code/quality/test_sample_sql.py
@@ -1,23 +1,54 @@
-from pyspark.sql import SparkSession, DataFrame
-import pytest
 import pandas as pd
+import pytest
+from pyspark.errors import AnalysisException
+from pyspark.sql import SparkSession
 
-# use src basically the root package to import all the test cases
-from src.com.code.quality.SampleSql import SampleSql
-
-ob = SampleSql()
-
-
-@pytest.fixture(scope="session")
-def spark_session():
-    return SparkSession.builder.getOrCreate()
+from src.com.code.quality.SqlExample import SqlExample
+from tests.com.code.quality.TestConfig import TestConfig
 
 
-def test_sql_to_spark(spark_session):
-    assert ob.sql_to_spark() == 1
+class TestSqlExample(TestConfig):
+    @pytest.fixture
+    def sql_example(self):
+        ob = SqlExample()
+        yield ob
 
+    def test_sql_to_spark(self, spark_fixture: SparkSession, sql_example: SqlExample):
+        assert sql_example.sql_to_spark() == 1
 
-def test_create_sql_table(spark_session):
-    d = [{"name": "Alice", "age": 1}, {"name": "Wonder", "age": 11}]
-    expected_pdf = pd.DataFrame(d)
-    assert expected_pdf.equals(ob.create_sql_table(d, "people"))
+    def test_create_read(self, spark_fixture: SparkSession, sql_example: SqlExample):
+        list = [{"name": "Alice", "age": 1}, {"name": "Wonder", "age": 11}]
+        expected_pdf = pd.DataFrame(list)
+        sql_example.create_sql_table(list, "people")
+
+        assert expected_pdf.equals(
+            sql_example.read_sql_table("SELECT name, age FROM people")
+        )
+        sql_example.drop_table("people")
+
+    def test_read_when_table_does_not_exists(
+        self, spark_fixture: SparkSession, sql_example: SqlExample
+    ):
+        try:
+            sql_example.read_sql_table("SELECT name, age FROM people")
+        except Exception as ex:
+            assert isinstance(ex, AnalysisException)
+
+    def test_join(self, spark_fixture: SparkSession, sql_example: SqlExample):
+        authors = [{"id": 0, "name": "Larry Wall"}]
+        books = [{"id": 0, "title": "Programming Perl"}]
+        authorship = [{"authorid": authors[0]["id"], "bookid": books[0]["id"]}]
+        sales = [{"bookid": books[0]["id"], "amount": 1000}]
+
+        sql_example.create_sql_table(authors, "author")
+        sql_example.create_sql_table(books, "book")
+        sql_example.create_sql_table(authorship, "authorship")
+        sql_example.create_sql_table(sales, "sales")
+        query: str = """SELECT amount FROM sales JOIN book ON sales.bookid=book.id JOIN authorship ON book.id = authorship.bookid JOIN author ON authorship.authorid = author.id"""
+
+        df: pd.DataFrame = sql_example.read_sql_table(query)
+        assert df.iloc[0, 0] == 1000
+        sql_example.drop_table("author")
+        sql_example.drop_table("book")
+        sql_example.drop_table("authorship")
+        sql_example.drop_table("sales")
